52313c8b52171e68a56baa959e1fdc47a6caaf62
Oliver Sandstrom <chasacademy-oliver-sandstrom@chasacademy.se>
2022-11-10 19:31:16 +0100

    fixed portfolio

--- timeline.js
+++ timeline.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 const createContentElement = (section) => {
 	const contentElement = document.createElement("div");
@@ -102,3 +102,20 @@ async function getJson() {
 	window.addEventListener('scroll', scrollHandler);
 })();
 
+document.addEventListener('DOMContentLoaded', function() {
+    var parent = document.querySelector('.splitview'),
+        topPanel = parent.querySelector('.top'),
+        handle = parent.querySelector('.handle'),
+        skewHack = 0,
+        delta = 0;
+
+    if (parent.className.indexOf('skewed') != -1) {
+        skewHack = 1000;
+    }
+
+    parent.addEventListener('mousemove', function(event) {
+        delta = (event.clientX - window.innerWidth / 2) * 0.5;
+        handle.style.left = event.clientX + delta + 'px';
+        topPanel.style.width = event.clientX + skewHack + delta + 'px';
+    });
+});
